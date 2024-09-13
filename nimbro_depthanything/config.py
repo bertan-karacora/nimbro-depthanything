86e5f6d299491a538ca03b03cab829c585c56e4c
Bertan Karacora <bertan.karacora@gmail.com>
2024-09-13 22:54:27 +0200

    Update config

--- nimbro_depthanything/config.py
+++ nimbro_depthanything/config.py
@@ -1,8 +1,33 @@
 MODELS = {
-    "depth_anything_v2_metric_hypersim_vits": {"encoder": "vits", "features": 64, "out_channels": [48, 96, 192, 384]},
-    "depth_anything_v2_metric_hypersim_vitb": {"encoder": "vitb", "features": 128, "out_channels": [96, 192, 384, 768]},
-    "depth_anything_v2_metric_hypersim_vitl": {"encoder": "vitl", "features": 256, "out_channels": [256, 512, 1024, 1024]},
-    "depth_anything_v2_metric_hypersim_vitg": {"encoder": "vitg", "features": 384, "out_channels": [1536, 1536, 1536, 1536]},
+    "depth_anything_v2_metric_hypersim_vits": {
+        "kwargs": {
+            "encoder": "vits",
+            "features": 64,
+            "out_channels": [48, 96, 192, 384],
+        },
+    },
+    "depth_anything_v2_metric_hypersim_vitb": {
+        "kwargs": {
+            "encoder": "vitb",
+            "features": 128,
+            "out_channels": [96, 192, 384, 768],
+        },
+    },
+    "depth_anything_v2_metric_hypersim_vitl": {
+        "kwargs": {
+            "encoder": "vitl",
+            "features": 256,
+            "out_channels": [256, 512, 1024, 1024],
+        },
+    },
+    "depth_anything_v2_metric_hypersim_vitg": {
+        "kwargs": {
+            "encoder": "vitg",
+            "features": 384,
+            "out_channels": [1536, 1536, 1536, 1536],
+        },
+    },
 }
-_PATH_DIR_WEIGHTS = "/root/colcon_ws/src/nimbro_depthanything/weights/"
-_PATH_DIR_ONNX = "/root/colcon_ws/src/nimbro_depthanything/onnx/"
+_PATH_DIR_WEIGHTS = "/root/colcon_ws/src/nimbro_depthanything/resources/weights/"
+_PATH_DIR_ONNX = "/root/colcon_ws/src/nimbro_depthanything/resources/models_onnx/"
+_PATH_DIR_ENGINES = "/root/colcon_ws/src/nimbro_depthanything/resources/engines/"
